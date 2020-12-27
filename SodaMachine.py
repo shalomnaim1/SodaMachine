e011f29de34c271d537ddfa6a2ea7b0023c4bc8c
Shalom Naim <shalom.naim@imperva.com>
2020-12-28 01:18:43 +0200

    sode machine with cash only

--- SodaMachine.py
+++ SodaMachine.py
@@ -1,51 +1,113 @@
 from typing import List, Tuple
 
 
-class Visa:
-    def withdraw(self, card_id, amount):
-        raise Exception("kokoko")
-
-
 class Drink:
-    def __init__(self, name, amount, price):
-        self.name = name
+    def __init__(self, amount, price):
         self.amount = amount
         self.price = price
 
     def get_one(self):
         self.amount -= 1
+
+        # if self.amount < 1:
+        #     print("Can't get a drink")
+        #     return False
+
         return True
 
     def load_more(self, amount):
         self.amount = amount
 
+        # if amount < 0:
+        #     print("Can't load negative amount of cans")
+        #     return False
+        # else:
+        #     self.amount += amount
+        #     return True
+
+    def __repr__(self):
+        return f"amount: {self.amount}, price: {self.price}"
+
 
 class SodaMachine:
     def __init__(self, drinks_mapping):
-        self.drinks_mapping = drinks_mapping
-        self.visa = Visa()
+        self.drinks = {}
+
+        for drink_name, drink_object in drinks_mapping.items():
+            self.drinks[drink_name] = drink_object
+
+    def add_new_kind_of_drink(self, drink_name, amount_of_cans, can_price):
+        self.drinks[drink_name] = Drink(amount_of_cans, can_price)
+        # if amount_of_cans >= 0 and can_price >= 0:
+        #     self._drinks[drink_name] = Drink(amount_of_cans, can_price)
+        # else:
+        #     raise Exception(
+        #         f"Invalid param supplied to start, {(drink_name, amount_of_cans, can_price)}, "
+        #         f"fail to start machine")
 
     @classmethod
     def start(cls, drinks_mapping: List[Tuple[str, int, int]]):
-        for drink_name, amount, price in drinks_mapping:
-            if amount < 0:
-                raise Exception(f"Invalid amount of {drink_name}")
+        instance = cls({})
 
-        mapping = {drink_name: Drink(drink_name, amount, price)
-                   for drink_name, amount, price in drinks_mapping}
-        
-        return cls(mapping)
+        for drink_name, amount_of_cans, can_price in drinks_mapping:
+            instance.add_new_kind_of_drink(drink_name, amount_of_cans, can_price)
 
-    def load_drinks(self, drink_name, amount):
-        self.drinks_mapping[drink_name] = amount
+        return instance
 
-    def buy_a_drink(self, name, currency=None, credit_card=None):
-        if credit_card:
-            if self.visa.withdraw(credit_card, self.drinks_mapping[name].price):
-                return name, 0
+    def load_drink_to_machine(self, drink_name, amount, price=None):
+        if drink_name in self.drinks:
+            self.drinks[drink_name].load_more(amount)
+        elif price is not None:
+            self.add_new_kind_of_drink(drink_name, amount, price)
+        print(self.drinks)
+
+    def buy_a_drink(self, name, cash_amount):
+        if name in self.drinks and cash_amount >= self.drinks[name].price:
+            self.drinks[name].get_one()
+            print(f"enjoy your {name} can, your change is {cash_amount - self.drinks[name].price}")
+            return True
         else:
-            if currency >= self.drinks_mapping[name].price and self.drinks_mapping[name].amount > 0:
-                self.drinks_mapping[name].get_one()
-                return name, currency - self.drinks_mapping[name].price
+            print("sorry, something when wrong, you cant have your soda")
+            return False
 
-        return None, None
+    def __str__(self):
+        return "\n".join(f"drink_name: {self.drinks[drink_name]}" for drink_name in self.drinks)
+
+
+def main():
+    my_machine = SodaMachine.start([("Coke", 10, 10), ("Orange Juice", 10,15), ("Water", 10, 7)])
+
+    while True:
+        print("Soda Machine:")
+        print("1. Buy a soda")
+        print("2. Load more soda cans")
+        print("3. Exit")
+        pick_action = input("Pick action: ")
+
+        if pick_action == "1":
+            print("Name, Price")
+            for drink_name in my_machine.drinks:
+                print(f"{drink_name}, {my_machine.drinks[drink_name].price}")
+
+            drink_name = input(f"What soda do you wanna have? ")
+
+            cash_amount = input(f"Enter amount of coins? ")
+            my_machine.buy_a_drink(drink_name, int(cash_amount))
+        elif pick_action == "2":
+            drink_name = input("What drink you want to load? ")
+            drink_amount = input("How many cans you have? ")
+
+            if drink_name not in my_machine.drinks:
+                price = input("This is a new produce, please enter price? ")
+                my_machine.load_drink_to_machine(drink_name, drink_amount, price)
+            else:
+                my_machine.load_drink_to_machine(drink_name, drink_amount)
+        elif pick_action == "3":
+            print("ByeBye, see ya next time")
+            break
+        else:
+            print("Invalid choose please try again...")
+
+
+if __name__ == "__main__":
+    main()
